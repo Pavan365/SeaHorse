cb74b12ee70a65ff5bb670dab14ba61e3a5c8103
harrykendell <harry@kendell.co.uk>
2023-07-03 15:15:31 +0100

    plans

--- main.cpp
+++ main.cpp
@@ -27,11 +27,20 @@ int main()
 
     //Time evolution
     /*
+    T is currently in x space as a central approx derivative.
+    It should also have have option to be in p basis, so diagonal. but be careful of fftfreq
+
+    Evolution, options to be:
+    - Split step -> X/2 * IFFT[ P * FFT[ X/2 * PSI(x) ]]
+    - Matrix multiplication with P in x basis. X/2 * P * X/2 * PSI(x)
+    - Which of X/P is half stepped
+    - Higher order error reduction (https://www.asc.tuwien.ac.at/~winfried/splitting/index.php?rc=0&ab=pp-34-a-ab&name=PP%203/4%20A)
+
     Take V function and T matrix for evolution
     Step updating psi with dt duration
     */
 
-//    Printing the H/T/V matrices
+    // Printing the H/T/V matrices
     
     plots(hs.x(),H0.V);
     for(int i=0;i<eig.cols();i++){
@@ -41,9 +50,7 @@ int main()
         plt::plot(hs.x(),40*vect.array()+energy);
         plt::text(xlim,energy,std::to_string(i));
     }
-
     plt::savefig("output/test.pdf"); // show the figure instead of saving it
-
     plt::show();
 
     std::cout<<(start-std::chrono::high_resolution_clock::now()).count()<<": Ran "<<dim<<"x"<<dim<<std::endl;
