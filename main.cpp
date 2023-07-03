0855f21e5135edc6f035863471fd3fb9a63ecd2d
harrykendell <harry@kendell.co.uk>
2023-07-03 15:31:34 +0100

    plans

--- main.cpp
+++ main.cpp
@@ -27,17 +27,31 @@ int main()
 
     //Time evolution
     /*
+    Parallelise? calc V's before doing all the steps
+    Or just build it into robustness checking evolutions
+
     T is currently in x space as a central approx derivative.
     It should also have have option to be in p basis, so diagonal. but be careful of fftfreq
 
-    Evolution, options to be:
-    - Split step -> X/2 * IFFT[ P * FFT[ X/2 * PSI(x) ]]
-    - Matrix multiplication with P in x basis. X/2 * P * X/2 * PSI(x)
-    - Which of X/P is half stepped
-    - Higher order error reduction (https://www.asc.tuwien.ac.at/~winfried/splitting/index.php?rc=0&ab=pp-34-a-ab&name=PP%203/4%20A)
-
-    Take V function and T matrix for evolution
-    Step updating psi with dt duration
+    Evolution (T(x/p)=P^2/2m,V(x)=potential), options to be:
+    - Split step
+        V(x)/2 * IFFT{ T(p) * FFT{ V(x)/2 * PSI(x)
+    - Matrix multiplication with T in x basis
+        V(x)/2 * T(x) * V(x)/2 * PSI(x)
+        prebake the T(x) as it doesnt change
+        Either matrix-wise IFFT*T(p)*FFT or central approx laplacian
+        FFt automatically gives periodic boundary conditions?
+        compare cost from making T once (n^3) and non-diag multiplication (n^3) vs FFT (n*log(n)) [n times] and diag multiplication (n^2?)
+    - Reverse split step order
+        can collapse some FFTs to optimise but slightly more to think about algorithmically
+        would we ever have PSI(p) normally?
+    - Higher order error reduction
+        Could help if we are doing lots of stepping and prebake P^2/2m(x)
+        (https://www.asc.tuwien.ac.at/~winfried/splitting/index.php?rc=0&ab=pp-34-a-ab&name=PP%203/4%20A)
+    - Eigenstate evolution?
+        exp(-i E_n dt) . PSI(eigenbasis)
+        exp(-i H dt) . PSI(x)
+        I guess with fixed potential it might be useful
     */
 
     // Printing the H/T/V matrices
