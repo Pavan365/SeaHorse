b357df88906101d378031fa51d9093558a428f0b
harrykendell <harry@kendell.co.uk>
2023-10-21 15:55:24 +0100

    mostly working RVec/CVec

--- gui.cpp
+++ gui.cpp
@@ -85,10 +85,54 @@ void CalcSpectrum(graph *plot, int num = 10)
 
     H0.calcSpectrum(num);
 
-    RVec test = (2.0+3i) * H0[0];
-    // std::cout<<"complex "<<test<<std::endl;
-
     for (auto i = 0; i < H0.spectrum.numEigvs; i++)plot->plot(x, H0[i] * 100 + H0.eigenvalue(i), "Eigenvector "+std::to_string(i));
+
+// TESTING
+    RVec rvec = H0[0];
+    CVec cvec = H0[0]*1i;
+
+    // RVec -> CVec
+    CVec t = rvec;
+
+    // RVec + double -> RVec
+    RVec t1 = rvec + 5.6;
+    // RVec * double -> RVec
+    RVec t2 = rvec * 5.6;
+    // RVec + complex -> CVec
+    RVec t3 = rvec * 5.6;
+    // Rvec * complex -> CVec
+    CVec t4 = rvec * 5.6i;
+
+    // CVec + double -> CVec
+    CVec t5 = cvec + 4.5;
+    // CVec * double -> CVec
+    CVec t6 = cvec * 4.5;
+    // CVec + complex -> CVec
+    CVec t7 = cvec + 4.5i;
+    // CVec * complex -> CVec
+    CVec t8 = cvec * 4.5i;
+
+    // RExpr + double -> RVec
+    RVec t9 = (rvec * 5.6) + 4.5;
+    // RExpr * double -> RVec
+    RVec t10 = (rvec * 5.6) * 4.5;
+    // RExpr + complex -> CVec
+    CVec t11 = (rvec * 5.6) + 4.5i;
+    // RExpr * complex -> CVec
+    CVec t12 = (rvec * 5.6) * 4.5i;
+
+    // CExpr + double -> CVec
+    CVec t13 = (cvec * 5.6) + 4.5;
+    // CExpr * double -> CVec
+    CVec t14 = (cvec * 5.6) * 4.5;
+    // CExpr + complex -> CVec
+    CVec t15 = (cvec * 5.6) + 4.5i;
+    // CExpr * complex -> CVec
+    CVec t16 = (cvec * 5.6) * 4.5i;
+
+    // auto test = x*x*x;
+    // auto test = rvec + cvec;
+
 }
 //------------------------------------------------------------------------------------
 // Program main entry point
--- include/Vectors.h
+++ include/Vectors.h
@@ -3,85 +3,177 @@
 #ifndef SEAHORSE_VECTORS
 #define SEAHORSE_VECTORS
 
-struct RVec
+template<typename ScalarType>
+struct Vec
 {
-    Eigen::VectorXd _vec;
+    using VecType = Eigen::Matrix<ScalarType, -1, 1>;
+
+    public:
+    VecType _vec;
 
     // Constructors
-    RVec() : _vec(Eigen::VectorXd()) {}
-    RVec(int dim) : _vec(Eigen::VectorXd::Zero(dim)) {}
-    RVec(int dim, double fill) : RVec(dim) { _vec.fill(fill); }
-    RVec(Eigen::VectorXd v) : _vec(v) {}
+    Vec() : _vec(VecType()) {} // Empty
+    Vec(int size) : _vec(Eigen::VectorXd::Zero(size)) {} // Zeros
+    Vec(int size, ScalarType fill) : Vec(size) { _vec.fill(fill); } // Filled
+    Vec(VecType v) : _vec(v) {} // Copied
 
     // Useful functions
-    const inline auto size() const { return _vec.size(); }
-    double &operator[](int i)
+    const inline int size() const { return _vec.size(); }
+    const ScalarType operator[](int i) const
     {
         if (i < 0 || i > _vec.size() - 1)
         {
-            std::cerr << "Out of Bounds indexing RVec" << std::endl;
-        }
-        return _vec[i];
-    }
-    const double operator[](int i) const
-    {
-        if (i < 0 || i > _vec.size() - 1)
-        {
-            std::cerr << "Out of Bounds indexing RVec" << std::endl;
+            std::cerr << "Out of Bounds indexing Vec" << std::endl;
         }
         return _vec[i];
     }
     const inline double norm(){return _vec.norm();}
+    const inline double conj(){return _vec.conj();}
 
-    // I'm not sure this actually works...
-    inline const auto operator^(const double d) const { return _vec.cwisePow(d); }
+    // Vec on Vec operations - same type
+    inline const auto operator+(Vec v) const { return _vec.array() + v._vec.array(); }
+    inline const auto operator-(Vec v) const { return _vec.array() + v._vec.array(); }
+    inline const auto operator*(Vec v) const { return _vec.array() * v._vec.array(); }
+    inline const auto operator/(Vec v) const { return _vec.array() / v._vec.array(); }
 
-    // Rvec on Rvec operations - if these are auto 
-    inline const auto operator+(RVec v) const { return _vec.array() + v._vec.array(); }
-    inline const auto operator-(RVec v) const { return _vec.array() + v._vec.array(); }
-    inline const auto operator*(RVec v) const { return _vec.array() * v._vec.array(); }
-    inline const auto operator/(RVec v) const { return _vec.array() / v._vec.array(); }
+    // Vec on double
+    inline const auto operator+(const double d) { return _vec.array() + d; }
+    inline const auto operator-(const double d) { return _vec.array() - d; }
+    inline const auto operator*(const double d) { return _vec.array() * d; }
+    inline const auto operator/(const double d) { return _vec.array() / d; }
 
-    // Casting Eigen's lazy things back to RVec
-    template <typename T, typename T2>
-    RVec(class Eigen::CwiseUnaryOp<T, T2> v) : RVec(v.eval()) {std::cout<<"\n"<<__PRETTY_FUNCTION__<<"\n"<<std::endl;}
-    template <typename T, typename T2, typename T3>
-    RVec(class Eigen::CwiseBinaryOp<T, T2, T3> v) : RVec(v.eval()) {std::cout<<"\n"<<__PRETTY_FUNCTION__<<"\n"<<std::endl;}
-    template <typename T, typename T2, typename T3, typename T4>
-    RVec(class Eigen::CwiseTernaryOp<T, T2, T3, T4> v) : RVec(v.eval()) {std::cout<<"\n"<<__PRETTY_FUNCTION__<<"\n"<<std::endl;}
+    // Vec on complex
+    inline const auto operator+(const std::complex<double> d) { return _vec.array() + d; }
+    inline const auto operator-(const std::complex<double> d) { return _vec.array() - d; }
+    inline const auto operator*(const std::complex<double> d) { return _vec.array() * d; }
+    inline const auto operator/(const std::complex<double> d) { return _vec.array() / d; }
 };
 
-// Eigens lazy operators on RVecs and doubles
+struct RVec : Vec<double>
+{
+    // Constructors
+    RVec() : Vec(Eigen::VectorXd()) {} // Empty
+    RVec(int size) : Vec(Eigen::VectorXd::Zero(size)) {} // Zeros
+    RVec(int size, double fill) : Vec(size,fill) {} // Filled
+    RVec(Eigen::VectorXd v) : Vec(v) {} // Copied
+
+    // Casts
+    // These are specialized to double so will be used with preference
+    // They take a lazy eigen expression which is purely real and allow an RVec to be made from it
+    template <template<typename> typename T, typename T2>
+    RVec(class Eigen::CwiseUnaryOp<T<double>, T2> v) : RVec((Eigen::VectorXd)v) {}
+    template <template < typename, typename> typename T, typename T2>
+    RVec(class Eigen::CwiseUnaryOp<T<double,double>, T2> v) : RVec((Eigen::VectorXd)v) {}
+    template <template < typename, typename, typename> typename T, typename T2>
+    RVec(class Eigen::CwiseUnaryOp<T<double,double,double>, T2> v) : RVec((Eigen::VectorXd)v) {}
+
+    template <template<typename> typename T, typename T2, typename T3>
+    RVec(class Eigen::CwiseBinaryOp<T<double>, T2, T3> v) : RVec((Eigen::VectorXd)v) {}
+    template <template < typename, typename> typename T, typename T2, typename T3>
+    RVec(class Eigen::CwiseBinaryOp<T<double,double>, T2, T3> v) : RVec((Eigen::VectorXd)v) {}
+    template <template < typename, typename, typename> typename T, typename T2, typename T3>
+    RVec(class Eigen::CwiseBinaryOp<T<double,double,double>, T2, T3> v) : RVec((Eigen::VectorXd)v) {}
+
+    template <template<typename> typename T, typename T2, typename T3, typename T4>
+    RVec(class Eigen::CwiseTernaryOp<T<double>, T2, T3, T4> v) : RVec((Eigen::VectorXd)v) {}
+    template <template < typename, typename> typename T, typename T2, typename T3, typename T4>
+    RVec(class Eigen::CwiseTernaryOp<T<double,double>, T2, T3, T4> v) : RVec((Eigen::VectorXd)v) {}
+    template <template < typename, typename, typename> typename T, typename T2, typename T3, typename T4>
+    RVec(class Eigen::CwiseTernaryOp<T<double,double,double>, T2, T3, T4> v) : RVec((Eigen::VectorXd)v) {}
+
+
+    // template <typename T, typename T2>
+    // explicit RVec(class Eigen::Product<T, T2> v) : RVec((VecType)v) {}
+};
+
+struct CVec : Vec<std::complex<double>>
+{
+    // Constructors
+    CVec() : Vec(Eigen::VectorXcd()) {} // Empty
+    CVec(int size) : Vec(Eigen::VectorXcd::Zero(size)) {} // Zeros
+    CVec(int size, double fill) : Vec(size,fill) {} // Filled
+    CVec(Eigen::VectorXcd v) : Vec(v) {} // Copied
+    // CVec(Eigen::VectorXd v) : Vec(v) {} // From Eigen
+    CVec(RVec v) : Vec(v._vec) {} // From Eigen
+
+    // Casts
+    // This uses enable_if to prevent clashing with RVecs when everything is real
+    // Casting Eigen's lazy things back to any CVec
+    template <template < typename> typename T, typename T2, typename D,
+      class = std::enable_if_t<std::is_same_v<std::complex<double>, D>>>
+    CVec(class Eigen::CwiseUnaryOp<T<D>, T2> v) : CVec((Eigen::VectorXcd)v) {}
+    template <template < typename, typename> typename T, typename T2, typename D, typename D2,
+      class = std::enable_if_t<std::is_same_v<std::complex<double>, D> || std::is_same_v<std::complex<double>, D2>>>
+    CVec(class Eigen::CwiseUnaryOp<T<D,D2>, T2> v) : CVec((Eigen::VectorXcd)v) {}
+    template <template < typename, typename, typename> typename T, typename T2, typename D, typename D2, typename D3,
+      class = std::enable_if_t<std::is_same_v<std::complex<double>, D> || std::is_same_v<std::complex<double>, D2> || std::is_same_v<std::complex<double>, D3>>>
+    CVec(class Eigen::CwiseUnaryOp<T<D,D2,D3>, T2> v) : CVec((Eigen::VectorXcd)v) {}
+
+    template <template < typename> typename T, typename T2, typename T3, typename D,
+      class = std::enable_if_t<std::is_same_v<std::complex<double>, D>>>
+    CVec(class Eigen::CwiseBinaryOp<T<D>, T2, T3> v) : CVec((Eigen::VectorXcd)v) {}
+    template <template < typename, typename> typename T, typename T2, typename T3, typename D, typename D2,
+      class = std::enable_if_t<std::is_same_v<std::complex<double>, D> || std::is_same_v<std::complex<double>, D2>>>
+    CVec(class Eigen::CwiseBinaryOp<T<D,D2>, T2, T3> v) : CVec((Eigen::VectorXcd)v) {}
+    template <template < typename, typename, typename> typename T, typename T2, typename T3, typename D, typename D2, typename D3,
+      class = std::enable_if_t<std::is_same_v<std::complex<double>, D> || std::is_same_v<std::complex<double>, D2> || std::is_same_v<std::complex<double>, D3>>>
+    CVec(class Eigen::CwiseBinaryOp<T<D,D2,D3>, T2, T3> v) : CVec((Eigen::VectorXcd)v) {}
+
+    template <template < typename> typename T, typename T2, typename T3, typename T4, typename D,
+      class = std::enable_if_t<std::is_same_v<std::complex<double>, D>>>
+    CVec(class Eigen::CwiseTernaryOp<T<D>, T2, T3, T4> v) : CVec((Eigen::VectorXcd)v) {}
+    template <template < typename, typename> typename T, typename T2, typename T3, typename T4, typename D, typename D2,
+      class = std::enable_if_t<std::is_same_v<std::complex<double>, D> || std::is_same_v<std::complex<double>, D2>>>
+    CVec(class Eigen::CwiseTernaryOp<T<D,D2>, T2, T3, T4> v) : CVec((Eigen::VectorXcd)v) {}
+    template <template < typename, typename, typename> typename T, typename T2, typename T3, typename T4, typename D, typename D2, typename D3,
+      class = std::enable_if_t<std::is_same_v<std::complex<double>, D> || std::is_same_v<std::complex<double>, D2> || std::is_same_v<std::complex<double>, D3>>>
+    CVec(class Eigen::CwiseTernaryOp<T<D,D2,D3>, T2, T3, T4> v) : CVec((Eigen::VectorXcd)v) {}
+};
+
+// Rightsided operators must be declared external to class
+// Eigen's lazy operators on RVecs and doubles
 inline const auto operator+(const RVec &v, const double d) { return v._vec.array() + d; }
 inline const auto operator-(const RVec &v, const double d) { return v._vec.array() - d; }
 inline const auto operator*(const RVec &v, const double d) { return v._vec.array() * d; }
 inline const auto operator/(const RVec &v, const double d) { return v._vec.array() / d; }
-inline const auto operator+(const double d, const RVec &v) { return d + v._vec.array(); }
-inline const auto operator-(const double d, const RVec &v) { return d - v._vec.array(); }
-inline const auto operator*(const double d, const RVec &v) { return d * v._vec.array(); }
-inline const auto operator/(const double d, const RVec &v) { return d / v._vec.array(); }
+// Eigen's lazy operators on RVecs and complex
+inline const auto operator+(const RVec &v, const std::complex<double> d) { return v._vec.array() + d; }
+inline const auto operator-(const RVec &v, const std::complex<double> d) { return v._vec.array() - d; }
+inline const auto operator*(const RVec &v, const std::complex<double> d) { return v._vec.array() * d; }
+inline const auto operator/(const RVec &v, const std::complex<double> d) { return v._vec.array() / d; }
+// Eigen's lazy operators on CVecs and doubles
+inline const auto operator+(const CVec &v, const double d) { return v._vec.array() + d; }
+inline const auto operator-(const CVec &v, const double d) { return v._vec.array() - d; }
+inline const auto operator*(const CVec &v, const double d) { return v._vec.array() * d; }
+inline const auto operator/(const CVec &v, const double d) { return v._vec.array() / d; }
+// Eigen's lazy operators on CVecs and complex
+inline const auto operator+(const CVec &v, const std::complex<double> d) { return v._vec.array() + d; }
+inline const auto operator-(const CVec &v, const std::complex<double> d) { return v._vec.array() - d; }
+inline const auto operator*(const CVec &v, const std::complex<double> d) { return v._vec.array() * d; }
+inline const auto operator/(const CVec &v, const std::complex<double> d) { return v._vec.array() / d; }
 
-// easy streaming of RVecs
-std::ostream &operator<<(std::ostream &os, const RVec &v) { return os << v._vec; }
 
-// Probably inherit from Vec -> both RVec and CVec
-// could be some sort of template thingy? Vec<double>==RVec, Vec<std::complex<double>>==CVec?
-// most functions will be the same but may make promoting to CVec hard?
-// can make: directly/complex+-*/RVec
-// struct CVec
-// {
-//     Eigen::VectorXcd _vec;
-// };
 
-// Allows maths style notation on vectors
+// easy streaming of Vecs
+template <typename ScalarType>
+std::ostream &operator<<(std::ostream &os, const Vec<ScalarType> &v) { return os << v._vec; }
+
+
+// Allow maths style notation on Vecs
+template <typename ScalarType>
+inline const auto exp(const Vec<ScalarType> &v) { return v._vec.array().exp(); }
+template <typename ScalarType>
+inline const auto abs2(const Vec<ScalarType> &v) { return v._vec.array().abs2(); }
+
+// These only really make sense on the Reals
 inline const auto cos(const RVec &v) { return v._vec.array().cos(); }
 inline const auto sin(const RVec &v) { return v._vec.array().sin(); }
-inline const auto exp(const RVec &v) { return v._vec.array().exp(); }
-inline const auto abs2(const RVec &v) { return v._vec.array().abs2(); }
-
 const auto box(const RVec &x, double min, double max)
 {
     return (x._vec.array().cwiseLess(max).cast<double>() + x._vec.array().cwiseGreater(min).cast<double>()) - 1;
 }
+
+
+
 
 #endif // SEAHORSE_VECTORS
