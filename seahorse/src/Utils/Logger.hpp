efc5960505422bb8cd1960891e891502c5b1149f
harrykendell <harry@kendell.co.uk>
2023-11-26 18:20:29 +0000

    Tried to fix colours again

--- seahorse/src/Utils/Logger.hpp
+++ seahorse/src/Utils/Logger.hpp
@@ -4,30 +4,28 @@
 
 #define S_INFO(...) S_INFO_IMPL(__VA_ARGS__);
 #define S_ERROR(...) S_ERR_IMPL("\033[1;91m[ERROR]\033[1;0m", __LINE__, __FILE__, __VA_ARGS__);
-#define S_FATAL(...)                                                    \
-    {                                                                   \
+#define S_FATAL(...)                                                      \
+    {                                                                     \
         S_ERR_IMPL("\033[1;91m[FATAL]", __LINE__, __FILE__, __VA_ARGS__); \
-        exit(EXIT_FAILURE);                                             \
+        exit(EXIT_FAILURE);                                               \
     };
 template <typename... Args>
 void S_ERR_IMPL(const char* type, int line, const char* fileName,
     Args&&... args)
 {
     std::ostringstream stream;
-    stream << type << " " << fileName << ":" << line << " -> ";
-    (stream << ... << std::forward<Args>(args));
-
-    std::cout << stream.str() << std::endl;
+    std::cout << type << " " << fileName << ":" << line << " -> ";
+    (std::cout << ... << std::forward<Args>(args));
+    std::cout << std::endl;
 }
 template <typename... Args>
 void S_INFO_IMPL(Args&&... args)
 {
     std::ostringstream stream;
 #ifndef NDEBUG
-    stream << "\033[1;93m[DEBUG MODE]\033[1;0m ";
+    std::cout << "\033[1;93m[DEBUG MODE]\033[1;0m ";
 #endif
-    stream << "\033[1;92m[INFO] \033[1;0m";
-    (stream << ... << std::forward<Args>(args));
-
-    std::cout << stream.str() << std::endl;
+    std::cout << "\033[1;92m[INFO] \033[1;0m";
+    (std::cout << ... << std::forward<Args>(args));
+    std::cout << std::endl;
 }
